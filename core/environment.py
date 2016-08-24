8f75d191cba6ff792c3ecd92ce8faa80d55d9282
hiraq <hiraq@onebitmedia.com>
2016-08-24 07:34:15 +0700

    Add comments to core environment function

--- core/environment.py
+++ core/environment.py
@@ -1,6 +1,17 @@
 import yaml
 
 def load_yaml_env(path):
+    """Load YAML Variables
+
+    Used to read yml files and load all key value
+    variables.
+
+    Args:
+        path (string) : A full path to yaml file
+
+    Returns:
+        A dictionary contains of all yaml key values
+    """
     output = ''
     with open(path, 'r') as stream:
         try:
