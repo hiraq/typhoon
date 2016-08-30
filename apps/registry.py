e8398612b8dd56bbbc99f92cd1432ff28afbcdf9
hiraq <hiraq@onebitmedia.com>
2016-08-30 09:10:39 +0700

    Remove momoko

--- apps/registry.py
+++ apps/registry.py
@@ -1,7 +1,6 @@
 from apps.hello.routes import app as HelloApp
 from apps.ping.routes import app as PingApp
 from apps.mongo.routes import app as MongoApp
-from apps.momoko.routes import app as MomokoApp
 from core.registry import Registry
 from core.exceptions.application import ContainerError
 
